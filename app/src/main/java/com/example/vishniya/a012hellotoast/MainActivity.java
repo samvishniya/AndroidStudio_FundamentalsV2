eff513f7a01a0d41787bbd8f1032fd483cc870b0
samvishniya <sam.vishniya@gmail.com>
2020-06-14 21:43:23 +0100

    Finished homework questions

--- app/src/main/java/com/example/vishniya/a012hellotoast/MainActivity.java
+++ app/src/main/java/com/example/vishniya/a012hellotoast/MainActivity.java
@@ -7,6 +7,23 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/*
+Q1 upon adding a 2nd activity to an app: the second activity xml layout file is created , a 2nd activity java class is added and the androidmanifestxml file is chnaged to declare a second activity
+
+
+Q2 If you remove the android:parentActivityNane and metadata elements from the androidmanifestxml file
+... the up button in the app bar no longer appears to send you back to the parent activity
+
+Q3 to create a new explicit intent, you need to use the following constructor method
+... new Intent(Context context, Class<?> class)
+
+Q4 In this homework hellotoast app you can add the current value of the count to the intent
+...using an intent extra
+
+Q5 In this homework app you can display the current count in the 2nd activity
+... by getting the activity that launched the event, getting the count from the getIntExtra, then applying that value to a textview
+
+ */
 
 
 
