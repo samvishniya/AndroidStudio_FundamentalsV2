0eadebf7d30386441263bae78283e76fe213a255
samvishniya <sam.vishniya@gmail.com>
2020-06-18 23:55:11 +0100

    Homework Completed

--- 02_2_Activity_Lifecycle_and_State/Homework/src/main/java/com/example/vishniya/homework/MainActivity.java
+++ 02_2_Activity_Lifecycle_and_State/Homework/src/main/java/com/example/vishniya/homework/MainActivity.java
@@ -2,12 +2,59 @@ package com.example.vishniya.homework;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/*
+Q1 If you dont add an override onsaveinstancestate , only editText preserves its state
+Q2 When a device config chagne happens e.g. rotation, android calls onpause, onstop and then ondestroy, and starts it over again calling oncreate, onstart and onresume( you can customise how it restarts)
+Q3 !!!!!!onsaveInstancestate is called just before the onstop method !!!!
+Q4 if you want to save data before an acitivty ends or is destroyed you should alter the methods of onpause or onstop
+
+ */
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView counter;
+    private int currentCount;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        counter = findViewById(R.id.text_counter);
+        editText = findViewById(R.id.editText_counter);
+    }
+
+    public void incrementCounter(View view) {
+        currentCount++;
+        if (counter != null) {
+            counter.setText(Integer.toString(currentCount));
+        }
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+
+        if (currentCount!=(0)) {
+            outState.putInt("count", currentCount);
+        }
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        if(savedInstanceState!=null){
+            currentCount=savedInstanceState.getInt("count");
+            counter.setText(Integer.toString(currentCount));
+        }
     }
 }
